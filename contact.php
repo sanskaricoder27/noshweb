a5df87377c37043f9e0d7ee6ad87ebddcaf4f8f1
Deepak Kumar <46528038+sanskaricoder27@users.noreply.github.com>
2022-05-04 23:41:40 +0530

    yes

--- contact.php
+++ contact.php
@@ -20,5 +20,24 @@ $data = "INSERT INTO info(Name, Email, Mobile, Hometown) VALUES ('$name','$email
 mysqli_query($connection,$data);
 header('location:successful.html');
 
+if(isset($_POST['submit'])){
+		$name=$_POST['Name'];
+		$email=$_POST['Email'];
+		$phone=$_POST['Mobile'];
+		$msg=$_POST['Hometown'];
+
+		$to='darshanam275@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+
 
 ?>
