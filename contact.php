63048a52e26b99c4d077662fc8cd91690a1dbd7c
Deepak Kumar <46528038+sanskaricoder27@users.noreply.github.com>
2022-05-04 23:45:17 +0530

    update

--- contact.php
+++ contact.php
@@ -1,24 +1,5 @@
 <?php
-$connection=mysqli_connect('localhost','root');
 
-if($connection){
-	echo("database connected");
-}
-else{
-	echo("database not connected");
-}
-mysqli_select_db($connection,'contactinfo');
-
-$name=$_POST['Name'];
-$email=$_POST['Email'];
-$mobile=$_POST['Mobile'];
-$hometown=$_POST['Hometown'];
-
-// echo("$name ,$email ,$mobile,$hometown");
-
-$data = "INSERT INTO info(Name, Email, Mobile, Hometown) VALUES ('$name','$email','$mobile','$hometown')";
-mysqli_query($connection,$data);
-header('location:successful.html');
 
 if(isset($_POST['submit'])){
 		$name=$_POST['Name'];
